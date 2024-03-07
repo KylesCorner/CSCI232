6bb26777b6e2132ed9979fd16895966fc06ffdf4
Kyle Krstulich <kylekrstulich@gmail.com>
2024-03-07 00:32:42 -0700

    finsihed autocomplete

---
+++ autocomplete/Term.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+
+// TODO: import stdin and stdout
+
+public class Term implements Comparable<Term> {
+    String query;
+    long weight;
+    
+    // Initializes a term with the given query string and weight.
+    public Term(String query, long weight){
+        if (query == null){
+            throw new java.lang.IllegalArgumentException("Queury can't be null");
+        }
+        if (weight < 0){
+            throw new java.lang.IllegalArgumentException("Weight has to be above 0");
+        }
+
+        this.query =query;
+        this.weight =weight;
+    }
+
+    public static Comparator<Term> byReverseWeightOrder() {
+        return new Comparator<Term>() {
+            public int compare(Term v, Term w) {
+
+                //some condition
+                //some condition
+                if(v.weight < w.weight){
+                    return 1;
+                }else if(v.weight > w.weight){
+                    return -1;
+                }
+                else{
+                //some condition
+                    return 0;
+                }
+
+            }
+
+        };
+    }
+
+    // Compares the two terms in lexicographic order but using only the first r characters of each query.
+    public static Comparator<Term> byPrefixOrder(int r) {
+
+        if(r<0){
+            throw new IllegalArgumentException();
+        }
+        return new Comparator<Term>() {
+            public int compare(Term v, Term w) {
+                int max = v.query.length() < w.query.length() ?
+                    v.query.length() : w.query.length();
+                max = max < r ? max :r;
+
+                for(int i = 0; i < max; i++){
+                    if(v.query.charAt(i) > w.query.charAt(i)) return 1;
+                    else if( v.query.charAt(i) < w.query.charAt(i)) return -1;
+                }
+                if(r > v.query.length() && w.query.length() > v.query.length()){
+                    return -1;
+                }
+                else if(r > w.query.length() && v.query.length() > w.query.length()){
+                    return -1;
+                }
+                return 0;
+            }
+        };
+    }
+
+    // Compares the two terms in lexicographic order by query.
+    public int compareTo(Term that) {
+        return query.compareTo(that.query);
+    }
+
+    // Returns a string representation of this term in the following format:
+    // the weight, followed by a tab, followed by the query.
+    public String toString() {
+        return Long.toString(weight) + "\t" + query;
+    }
+
+    // unit testing (you should have some Unit Testing here to confirm that your methods work); for example...
+    public static void main(String[] args) {
+        Term[] terms = new Term[5];
+        terms[0] = new Term("Trevor", 45);
+        terms[1] = new Term("Kathy", 43);
+        terms[2] = new Term("Ellie", 11);
+        terms[3] = new Term("Allen", 9);
+        terms[4] = new Term("Eva", 1);
+        
+        Arrays.sort(terms);
+        for (Term t : terms) {
+            StdOut.println(t);
+        }
+        
+        StdOut.println("");
+        Arrays.sort(terms, Term.byReverseWeightOrder());
+        for (Term t : terms) {
+            StdOut.println(t);
+        }
+        
+        StdOut.println("");
+        Arrays.sort(terms, Term.byPrefixOrder(1));
+        for (Term t : terms) {
+            StdOut.println(t);
+        }        
+    }
+}
---
+++ autocomplete/readme.txt
@@ -0,0 +1,68 @@
+/******************************************************************************
+ *  Name:
+ *
+ *  Hours to complete assignment (optional):
+ *
+ ******************************************************************************/
+
+Programming Assignment 2: Autocomplete
+
+
+/******************************************************************************
+ *  Describe how your firstIndexOf() method in BinarySearchDeluxe.java
+ *  finds the first index of a key that equals the search key.
+ *****************************************************************************/
+
+
+
+
+/******************************************************************************
+ *  What is the order of growth of the number of compares (in the
+ *  worst case) that each of the operations in the Autocomplete
+ *  data type make, as a function of the number of terms n and the
+ *  number of matching terms m?  (Big-Oh notation)
+ *
+ *  Recall that with order-of-growth notation, you should discard
+ *  leading coefficients and lower-order terms, e.g., m^2 + m log n.
+ *****************************************************************************/
+
+constructor:
+
+allMatches():
+
+numberOfMatches():
+
+
+
+
+/******************************************************************************
+ *  Known bugs / limitations.
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *  Describe whatever help (if any) that you received.
+ *  Don't include readings or lectures, but do include
+ *  any help from people (including course staff, lab TAs,
+ *  classmates, and friends) and attribute them by name.
+ *
+ *  Also include any resources (including the web) that you may
+ *  may have used in creating your design.
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *  Describe any serious problems you encountered.
+ *****************************************************************************/
+
+
+
+
+
+/******************************************************************************
+ *  List any other comments here. Feel free to provide any feedback
+ *  on how much you learned from doing the assignment, and whether
+ *  you enjoyed doing it.
+ *****************************************************************************/
+
+
